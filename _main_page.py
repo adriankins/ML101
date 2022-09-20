0af4d7e09ca225a873c7656a0dddaa295e68f7df
adriankins <113998484+adriankins@users.noreply.github.com>
2022-09-20 16:29:48 +0200

    Update _main_page.py

--- _main_page.py
+++ _main_page.py
@@ -9,12 +9,10 @@ import streamlit  as st
 st.title("my magic title")
 my_text = st.text("A random magic text for you")
 my_button = st.button("Run ML computation")
-my_date = st.date_input(
-    "When's your birthday",
-    datetime.date(2019, 7, 6))
+#my_date = st.date_input("When's your birthday",datetime.date(2019, 7, 6))
 
 
 if my_button:
     st.title("The model is trunning ...")
-if my_date:
-    st.write('Your birthday is:', d)
+#if my_date:
+#    st.write('Your birthday is:', d)
